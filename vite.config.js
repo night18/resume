ff9bb92af43a31ebd527ec6d8ddbd8f584ea6803
Chun-Wei Chiang <chiang@chunwei.org>
2022-09-18 16:48:10 -0400

    base url

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  publicPath: process.env.NODE_ENV === 'production'? "/resume/": "/",
   plugins: [vue()],
   build: {
     commonjsOptions: {
